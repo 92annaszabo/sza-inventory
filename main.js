d7843bb2626b5a985eaf734bdc491da0cf6fce8b
dkerek <kerek.daniel@telekom.hu>
2020-02-19 10:19:31 +0100

    header with handlebars partials

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
 const express = require('express')
-const Handlebars = require('express-handlebars')
+const handlebars = require('express-handlebars')
 const app = express()
 const port = process.env.port || 3000
 
 const absPath='C:/Users/szabo1anna130/Desktop/braininghub/sza-inventory/public'
 
-app.engine('handlebars', Handlebars());
+
+
+
+app.engine( 'handlebars', handlebars( {
+    extname: 'handlebars',
+    defaultView: 'default',
+    // layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/partials/'
+}));
+
 app.set('view engine', 'handlebars');
-//Handlebars.registerPartial("header", "{{header}}");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
 app.get('/products', (req, res)=> {
-    res.render('products')
+    title = "Termékek"
+    res.render('products', {title: title})
+})
+
+app.get('/storage', (req, res)=> {
+    title = "Készlet"
+    res.render('storage', {title: title})
+})
+
+app.get('/groups', (req, res)=> {
+    title = "Csoportok"
+    res.render('groups', {title: title})
 })
 
 
